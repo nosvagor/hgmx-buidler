37946f790616b732bcc419f3b70689160cab3cfd
cullyn <cullyn@trendcapital.com>
2025-05-20 19:50:15 -0700

    wip: blueprint

---
+++ internal/server/routes.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/nosvagor/hgmx-builder/internal/web/home"
+	"github.com/nosvagor/hgmx-builder/internal/web/palette"
+	"github.com/nosvagor/hgmx-builder/internal/web/settings"
+)
+
+func RegisterRoutes(e *echo.Echo) {
+	// Home
+	e.GET("/", home.Main)
+
+	// Settings
+	e.GET("/settings", settings.Main)
+
+	// Palette
+	e.GET("/palette", palette.Main)
+}
+
+func RegisterStaticRoutes(e *echo.Echo) {
+	e.Static("/app/static", "static")
+
+	// Files
+	e.File("/favicon.ico", "static/favicon/favicon.ico")
+}
